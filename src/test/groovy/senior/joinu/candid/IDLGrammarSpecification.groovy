0410ae1f248e58e087dbe26b4db415355239e0b3
Thomas Glaeser <tglaeser@users.noreply.github.com>
2020-07-26 14:29:08 -0400

    Moving towards parameterized unit test

--- src/test/groovy/senior/joinu/candid/IDLGrammarSpecification.groovy
+++ src/test/groovy/senior/joinu/candid/IDLGrammarSpecification.groovy
@@ -5,32 +5,45 @@ import com.squareup.kotlinpoet.FileSpec
 import senior.joinu.candid.transpile.KtTranspiler
 import senior.joinu.candid.transpile.TranspileContext
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * System under specification: {@link IDLGrammar}.
  * @author tglaeser
  */
 class IDLGrammarSpecification extends Specification {
-    def 'test'() {
+    @Unroll def 'test service method #methodName'() {
         given: 'a test fixture'
-        String idl = """
-            service : {
-              "greet": (text) -> (text);
-            }
-        """
+        String idl = "service : { $methodName: (${getVarargs(arguments)}) -> (${getVarargs(results)}); }"
+        println(">>> IDL"); println(idl); println('')
+        List<IDLFuncAnn> annotations = []
+        IDLMethodType methodType = new IDLType.Reference.Func(arguments.collect { new IDLArgType(null, it) }, results.collect { new IDLArgType(null, it) }, annotations)
+        IDLMethod method = new IDLMethod(methodName, methodType)
+        List<IDLDef.Import> imports = []
+        List<IDLDef.Type> types = []
+        List<IDLMethod> methods = [method]
+        IDLActorDef actor = new IDLActorDef(null, new IDLType.Reference.Service(methods))
+        IDLProgram program = new IDLProgram(imports, types, actor)
 
-        when: 'the program source is generated from the IDL'
-        IDLProgram program = GrammarKt.parseToEnd(IDLGrammar.INSTANCE, idl)
+        when: 'the Kotlin source is generated from the IDL'
+        IDLProgram result = GrammarKt.parseToEnd(IDLGrammar.INSTANCE, idl)
 
-        and: 'printed to standard out for no good reason'
-        println(">>> IDL $idl")
-        TranspileContext context = KtTranspiler.INSTANCE.transpile(program, "tdl.domain.etc", "Test.kt")
+        and: 'printed to standard output for no good reason'
+        TranspileContext context = KtTranspiler.INSTANCE.transpile(result, "tld.d.etc", "Test.kt")
         FileSpec spec = context.currentSpec.build()
         println("<<< Kotlin")
         spec.writeTo(System.out)
 
         then: 'the program matches the expectation'
-        program.actor.type in IDLType.Reference.Service
         noExceptionThrown()
+        result == program
+
+        where: 'the service signature is as defined here'
+        methodName | arguments                         | results
+        'greet'    | [IDLType.Primitive.Text.INSTANCE] | [IDLType.Primitive.Text.INSTANCE]
+    }
+
+    static String getVarargs(List<IDLType> args) {
+        return args.collect { it.toString().toLowerCase() }.join(' ,')
     }
 }
