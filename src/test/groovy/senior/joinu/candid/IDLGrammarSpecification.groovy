cf80bd582fb7479318192033f5123ce9391c1029
Thomas Glaeser <tglaeser@users.noreply.github.com>
2020-08-08 11:31:21 -0400

    Moving text keywords and pattern definition to the corresponding types ... adding fields text/pattern to each type as well as a #toString implementation ... unit testing it

--- src/test/groovy/senior/joinu/candid/IDLGrammarSpecification.groovy
+++ src/test/groovy/senior/joinu/candid/IDLGrammarSpecification.groovy
@@ -15,8 +15,9 @@ class IDLGrammarSpecification extends Specification {
     @Unroll def 'test service method #methodName'() {
         given: 'a test fixture'
         String idl = """
+            ${getTypes(types)}
             service : {
-               $methodName: (${getVarargs(arguments)}) -> (${getVarargs(results)});
+               "$methodName": (${getVarargs(arguments)}) -> (${getVarargs(results)});
             }
         """.stripIndent()
         println(">>> IDL $idl")
@@ -24,7 +25,6 @@ class IDLGrammarSpecification extends Specification {
         IDLMethodType methodType = new IDLType.Reference.Func(arguments.collect { new IDLArgType(null, it) }, results.collect { new IDLArgType(null, it) }, annotations)
         IDLMethod method = new IDLMethod(methodName, methodType)
         List<IDLDef.Import> imports = []
-        List<IDLDef.Type> types = []
         List<IDLMethod> methods = [method]
         IDLActorDef actor = new IDLActorDef(null, new IDLType.Reference.Service(methods))
         IDLProgram program = new IDLProgram(imports, types, actor)
@@ -32,25 +32,63 @@ class IDLGrammarSpecification extends Specification {
         when: 'the Kotlin source is generated from the IDL'
         IDLProgram result = GrammarKt.parseToEnd(IDLGrammar.INSTANCE, idl)
 
+        then: 'the program matches the expectation'
+        noExceptionThrown()
+        result == program
+
         and: 'printed to standard output for no good reason'
         TranspileContext context = KtTranspiler.INSTANCE.transpile(result, "tld.d.etc", "Test.kt")
         FileSpec spec = context.currentSpec.build()
         println("<<< Kotlin")
         spec.writeTo(System.out)
 
-        then: 'the program matches the expectation'
-        noExceptionThrown()
-        result == program
-
         where: 'the service signature is as defined here'
-        methodName      | arguments                         | results
-        'greet'         | [IDLType.Primitive.Text.INSTANCE] | [IDLType.Primitive.Text.INSTANCE]
-        'configure'     | [IDLType.Primitive.Text.INSTANCE] | []
+        methodName      | arguments                                                                                     | results
+        'greet'         | [IDLType.Primitive.Text.INSTANCE]                                                             | [IDLType.Primitive.Text.INSTANCE]
+        'configure'     | [IDLType.Primitive.Text.INSTANCE]                                                             | []
+        'get'           | [new IDLType.Constructive.Vec(IDLType.Primitive.Nat8.INSTANCE)]                               | [new IDLType.Constructive.Opt(new IDLType.Constructive.Vec(IDLType.Primitive.Nat8.INSTANCE))]
+        'put'           | [new IDLType.Constructive.Vec(IDLType.Primitive.Nat8.INSTANCE), new IDLType.Constructive.Vec(IDLType.Primitive.Nat8.INSTANCE)] | [IDLType.Primitive.Bool.INSTANCE]
+        'getInHex'      | [IDLType.Primitive.Text.INSTANCE]                                                             | [new IDLType.Constructive.Opt(IDLType.Primitive.Nat8.INSTANCE)]
+        'putInHex'      | [IDLType.Primitive.Text.INSTANCE, IDLType.Primitive.Text.INSTANCE]                            | [IDLType.Primitive.Bool.INSTANCE]
+        'getWithTrace'  | [new IDLType.Constructive.Vec(IDLType.Primitive.Nat8.INSTANCE), new IDLType.Id('Bucket')]     | [new IDLType.Constructive.Opt(new IDLType.Constructive.Vec(IDLType.Primitive.Nat8.INSTANCE))]
+        'putWithTrace'  | [new IDLType.Constructive.Vec(IDLType.Primitive.Nat8.INSTANCE), new IDLType.Constructive.Vec(IDLType.Primitive.Nat8.INSTANCE), new IDLType.Id('Bucket')]     | [IDLType.Primitive.Bool.INSTANCE]
+        'initialize'    | []                                                                                            | []
+        'peers'         | []                                                                                            | [new IDLType.Id('List_2')]
+        'ping'          | []                                                                                            | []
+        'size'          | []                                                                                            | [IDLType.Primitive.Natural.INSTANCE]
+        'whoami'        | []                                                                                            | [IDLType.Primitive.Natural.INSTANCE]
+        types << [
+                [],
+                [],
+                [],
+                [],
+                [],
+                [],
+                [
+                    new IDLDef.Type('Key', new IDLType.Constructive.Record([new IDLFieldType('preimage', new IDLType.Constructive.Vec(IDLType.Primitive.Nat8.INSTANCE), UtilsKt.idlHash('preimage')), new IDLFieldType('image', new IDLType.Constructive.Vec(IDLType.Primitive.Nat8.INSTANCE), UtilsKt.idlHash('image'))])),
+                    new IDLDef.Type('List', new IDLType.Constructive.Opt(new IDLType.Constructive.Record([new IDLFieldType(null, new IDLType.Id('Key'), 0), new IDLFieldType(null, new IDLType.Id('List'), 1)]))),
+                    new IDLDef.Type('Bucket', new IDLType.Id('List'))
+                ],
+                [
+                    new IDLDef.Type('Key', new IDLType.Constructive.Record([new IDLFieldType('preimage', new IDLType.Constructive.Vec(IDLType.Primitive.Nat8.INSTANCE), UtilsKt.idlHash('preimage')), new IDLFieldType('image', new IDLType.Constructive.Vec(IDLType.Primitive.Nat8.INSTANCE), UtilsKt.idlHash('image'))])),
+                    new IDLDef.Type('List', new IDLType.Constructive.Opt(new IDLType.Constructive.Record([new IDLFieldType(null, new IDLType.Id('Key'), 0), new IDLFieldType(null, new IDLType.Id('List'), 1)]))),
+                    new IDLDef.Type('Bucket', new IDLType.Id('List'))
+                ],
+                [],
+                [
+                    new IDLDef.Type('List_2', new IDLType.Constructive.Opt(new IDLType.Constructive.Record([new IDLFieldType(null, IDLType.Primitive.Text.INSTANCE, 0), new IDLFieldType(null, new IDLType.Id('List_2'), 1)]))),
+                ],
+                [],
+                [],
+                [],
+        ]
     }
 
     private static String getVarargs(List<IDLType> args) {
-        return args.collect { //TODO :: Need a better type-token mapping approach
-            it.toString().toLowerCase()
-        }.join(' ,')
+        return args.collect { it.toString() }.join(', ')
+    }
+
+    private static String getTypes(List<IDLDef.Type> types) {
+        return types.collect { it.toString() }.join(System.getProperty('line.separator'))
     }
 }
